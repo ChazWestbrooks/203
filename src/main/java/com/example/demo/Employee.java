b623c86b2567aab8421c207cf633e27da84aabed
Chaz <chazwestbrooks@gmail.com>
2018-11-14 16:04:28 -0500

    Initial commit

---
+++ src/main/java/com/example/demo/Employee.java
@@ -0,0 +1,49 @@
+package com.example.demo;
+
+public class Employee {
+    private long id;
+    private String Fname;
+    private String Lname;
+    private String SSN;
+    private String DOB;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getFname() {
+        return Fname;
+    }
+
+    public void setFname(String fname) {
+        Fname = fname;
+    }
+
+    public String getLname() {
+        return Lname;
+    }
+
+    public void setLname(String lname) {
+        Lname = lname;
+    }
+
+    public String getSSN() {
+        return SSN;
+    }
+
+    public void setSSN(String SSN) {
+        this.SSN = SSN;
+    }
+
+    public String getDOB() {
+        return DOB;
+    }
+
+    public void setDOB(String DOB) {
+        this.DOB = DOB;
+    }
+}
